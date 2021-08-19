5f7ff8e4da3d0ec4cc44cc3d7bdc97c468e22a9c
JvoProgramming <jvoprogramming@gmail.com>
2021-08-19 16:04:04 -0700

    Basic nav menu/route functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Project from './pages/Projects';
 
 export default function App() {
   return (
     <>
-      <h1>Welcome to react portfolio Project</h1>
-      <h3>-Johnny Vo</h3>
+      <Router>
+        <NavMenu />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Project />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
