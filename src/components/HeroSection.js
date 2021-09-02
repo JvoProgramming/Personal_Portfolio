17f52bb986af2d83a512e63460f7b2f398d8769b
JvoProgramming <JvoProgramming@gmail.com>
2021-09-02 12:43:24 -0700

    added navmenu

---
+++ src/components/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeroImg from '../assets/images/hero.png';
+
+export default function HeroSection() {
+  return (
+    <div>
+      <div>
+        <h1>
+          <span>Hello, This is</span>
+          <span>Johnny Vo</span>
+        </h1>
+        <div className="hero__img">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
